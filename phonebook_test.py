5b5e5d87854ffbfc1aae9babd381c4a7639b5cb6
Baliqis Sulaiman <151347697+bally3399@users.noreply.github.com>
2024-02-05 13:35:11 +0100

    Add files via upload

---
+++ phonebook_test.py
@@ -0,0 +1,36 @@
+import unittest
+from phone import Phone
+class TestContactFunction(unittest.TestCase):
+    def setUp(self):
+        self.phone = Phone()
+
+    def test_add_contact(self):
+        self.phone.add_contact("John", "08105795528")
+        self.assertEqual(self.phone.contacts["John"], "08105795528")
+
+    def test_delete_contact(self):
+        self.phone.add_contact("jane", "08106795528")
+        self.phone.delete_contact("john")
+        self.assertNotIn("Jane", self.phone.contacts, "Contact not deleted successfully.")
+
+    def test_display_contacts(self):
+        self.phone.add_contact("Ruth", "1111111111")
+        self.phone.add_contact("Esther", "2222222222")
+        contacts = self.phone.display_contacts()
+        self.assertEqual(len(contacts), 2, "Incorrect number of contacts.")
+        self.assertIn("Ruth", contacts, "Contact 'Ruth' not found.")
+        self.assertIn("Esther", contacts, "Contact 'Esther' not found.")
+
+    def test_dial_contact(self):
+        self.phone.add_contact("Bally", "08105795528")
+        dial_result = self.phone.dial_contact("Bally")
+        expected_result = "Dialing Bally: 08105795528"
+        self.assertEqual(dial_result, expected_result, f"Expected: {expected_result}, Got: {dial_result}")
+
+        dial_result_not_found = self.phone.dial_contact("Unknown")
+        expected_result_not_found = "Contact Unknown not found."
+        self.assertEqual(dial_result_not_found, expected_result_not_found, f"Expected: {expected_result_not_found}, Got: {dial_result_not_found}")
+
+if __name__ == '__main__':
+    unittest.main()
+
