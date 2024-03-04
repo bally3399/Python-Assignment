34324ed7161a907e080377626520a3d2d1d19e00
Baliqis Sulaiman <151347697+bally3399@users.noreply.github.com>
2024-03-04 09:17:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ from user import User
 class Main:
     def __init__(self):
         self.user = User()
-        self.diary = Diary("username", "password")
+        self.diary = None
 
     def display(self):
         print("********************************")
         print("Welcome to my Diary")
-        print("1.Create Entry\n 2.Unlock\n 3.Lock\n 4.Check if Dairy is locked\n 5.Find Entry\n 6.Delete Entry\n "
-              "7.Update\n 8.Exit App")
+        print("1.Create Diary\n2.Create Entry\n3.Unlock\n4.Lock\n5.Check if Dairy is locked\n6.Find Entry\n"
+              "7.Delete Entry\n8.Update\n9.Exit App")
         print("********************************")
         choice = input("Enter your choice: ")
         if choice == "1":
@@ -37,22 +37,27 @@ class Main:
         self.display()
 
     def create_entry(self):
-        title = input("Enter title: ")
-        body = input("Enter body: ")
-        self.user.add_entry(title, body)
-        print("Entry created successfully!!")
-        self.display()
+        if self.diary is not None:
+            title = input("Enter title: ")
+            body = input("Enter body: ")
+            self.user.add_entry(title, body)
+            print("Entry created successfully!!")
+        elif self.diary is None:
+            print("You have to create entry")
+            self.display()
 
     def unlock(self):
+        self.diary = Diary("Username", "1234")
         password = str(input("Enter your password: "))
         if self.diary.password == password:
             self.user.unlock_diary(password)
             print("You have successfully unlock dairy")
         else:
-            print("Incorrect Password")
+            print("Incorrect Password\nCreate entry first")
         self.display()
 
     def lock(self):
+        self.diary = Diary("Username", "1234")
         password = str(input("Enter your password: "))
         if self.diary.password == password:
             self.user.lock_diary()
@@ -60,9 +65,9 @@ class Main:
         else:
             print("Incorrect Password")
         self.display()
-        pass
 
     def check_if_diary_is_locked(self):
+        self.diary = Diary("Username", "1234")
         password = str(input("Enter your password: "))
         if self.diary.password == password:
             if self.diary.is_locked():
@@ -74,18 +79,21 @@ class Main:
         self.display()
 
     def find_entry(self):
+        self.diary = Diary("Username", "1234")
         id_number = input("Enter your id: ")
         self.diary.find_entry(id_number)
         print("Entry found for ID: " + id_number)
         self.display()
 
     def delete_entry(self):
+        self.diary = Diary("Username", "1234")
         id_number = input("Enter your id")
         self.diary.delete_entry(id_number)
         print("Entry delete successfully")
         self.display()
 
     def update_entry(self):
+        self.diary = Diary("Username", "1234")
         id_number = input("Enter your id: ")
         title = str(input("Enter title: "))
         body = str(input("Enter body: "))
@@ -94,13 +102,15 @@ class Main:
         self.display()
 
     def create_diary(self):
+        self.diary = Diary("Username", "1234")
         name = input("Enter your name: ")
         password = input("Enter your password: ")
         self.user.create_diary(name, password)
         print("You have successfully create diary")
         self.display()
 
-    def exit(self):
+    @staticmethod
+    def exit():
         print("Exit")
         return
 
@@ -112,3 +122,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    
